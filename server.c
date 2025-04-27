703f2f1e020638a0be60ac69f2e1c79d8fe9b608
Ali Zeitoun <alimohamedzeitoun@gmail.com>
2025-04-27 06:47:57 -0400

    fix compiling errors by including relevant header files and add make the processes repeat

--- server.c
+++ server.c
@@ -1,21 +1,31 @@
+#include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <sys/select.h>
-#define MAXSIZE 80
+#define MAXSIZE 4096
 
 
 /** 
-* Writes message in buff to all client file descriptors in writefds
-*/
-int write_to_clients(char *buff, fd_set writefds) {
-
+ * Writes message in buff to all client file descriptors in openfds except the writingfd
+ */
+void write_to_clients(char *buf, fd_set *openfds, int num_bytes, int maxfd, int writingfd) {
+    int num_bytes_written;
+    for(int i = 0; i < maxfd + 1; i++) {
+        if(i != writingfd && FD_ISSET(i, openfds)) {
+            num_bytes_written = write(i, buf, num_bytes);
+        }
+        // some sort of error handling should be done here
+    }
 }
 
 int main(int argc, char **argv) {
-
-    char buff[MAXSIZE];
+    
+    fd_set openfds;
+    fd_set readfds;
+    char buf[MAXSIZE];
 
     // server socket configuration
     int listen_soc = socket(AF_INET, SOCK_STREAM, 0);
@@ -43,51 +53,59 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    fd_set readfds;
     FD_ZERO(&readfds);
     int max_fd = 0;
     int result = fork();
     // accepting connections and communication between server
-
+    
     if(result == 0) { // process for reading messages from clients and writing them to the rest of the clients
         if(close(listen_soc) == -1) {
             perror("close");
             exit(1);
         }
 
-        int num_ready = select(max_fd, &readfds, NULL, NULL, NULL);
-        if(num_ready == -1) {
-            perror("select");
-            exit(1);
-        }
-
-        for(int i = 0; i < max_fd; i++) {
-            if(FD_ISSET(i, &readfds)) {
-                int num_bytes_read = read(i, buff, MAXSIZE);
-                if(num_bytes_read == -1) {
-                    perror("read");
-                    exit(1);
-                }
-                write_to_clients();
+        while(1) {
+            readfds = openfds;
+            int num_ready = select(max_fd + 1, &readfds, NULL, NULL, NULL);
+            if(num_ready == -1) {
+                perror("select");
+                exit(1);
             }
+
+            for(int i = 0; i < max_fd; i++) {
+                if(FD_ISSET(i, &readfds)) {
+                    int num_bytes_read = read(i, buf, MAXSIZE);
+                    if(num_bytes_read == -1) {
+                        perror("read");
+                        exit(1);
+                    }
+                    write_to_clients(buf, &openfds, num_bytes_read, max_fd, i);
+                }
+            }
+        
         }
+        
 
     }else { // process for accepting connections
-        struct sockaddr_in client_addr;
-        client_addr.sin_family = AF_INET;
-        int len = sizeof(struct sockaddr_in);
-        int client_soc = accept(listen_soc, (struct sockaddr_in *) &client_addr, &len);
-    
-        if(client_soc == -1) {
-            perror("accept");
-            close(listen_soc);
-            exit(1);
-        }
+        while(1) {
+            struct sockaddr_in client_addr;
+            client_addr.sin_family = AF_INET;
+            int len = sizeof(struct sockaddr_in);
+            int client_soc = accept(listen_soc, (struct sockaddr *) &client_addr, &len);
+        
+            if(client_soc == -1) {
+                perror("accept");
+                close(listen_soc);
+                exit(1);
+            }
 
-        FD_SET(client_soc, &readfds);
-        if(client_soc > max_fd) {
-            max_fd = client_soc;
+            FD_SET(client_soc, &openfds);
+            if(client_soc > max_fd) {
+                max_fd = client_soc;
+            }
+
         }
+        
         
     }
     
